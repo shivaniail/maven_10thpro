2a322b681aff0cfe4cc3f8d7d582a8e9335848ce
shivaniail <shivanicail2004@gmail.com>
2025-04-24 14:40:27 +0530

    first commit

---
+++ src/main/java/com/program4/www/program4/App.java
@@ -0,0 +1,13 @@
+package com.program4.www.program4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
